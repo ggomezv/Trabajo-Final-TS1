d5d202995d7f4a939cc73e57389f631fc11e3abb
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-28 08:18:53 -0500

    NUEVA ENTRADA

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,13 +1,16 @@
 G=[]
 H=[]
 #Entrada
-x = int(input("ingrese el numero de jugadores"))
+x = int(input("ingrese el numero de jugadores: "))
 
 while True:
-    y = input("ingrese su nombre")
+    y = input("ingrese su nombre: ")
     G.append(y)
-    z=int(input("ingrese numero de cartillas"))
-    H.append(z)
-    if len(G) == x and len(H) == z:
+    z=int(input("ingrese numero de cartillas: "))
+    if z <=3:
+        H.append(z)
+    else:
+        z = int(input("Ingrese numero de cartillas: "))
+    if len(G) == x:
         break
 
